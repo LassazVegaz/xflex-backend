499978841555bec71e2dd2cbac6015dea2af3670
LassazVegaz <lasindunuwanga@gmail.com>
2022-08-29 23:57:05 +0530

    sup routes fixes

--- routes/suppliersRouter.js
+++ routes/suppliersRouter.js
@@ -3,28 +3,6 @@ const suppliersController = require("../controllers/suppliersCtrl");
 
 const router = Router();
 
-// get supplier by id
-router.get("/:id", async (req, res, next) => {
-	try {
-		const { id } = req.params;
-		const supplier = await suppliersController.getSupplierById(id);
-		res.status(200).json(supplier);
-	} catch (error) {
-		next(error);
-	}
-});
-
-// search suppliers
-router.get("/", async (req, res, next) => {
-	try {
-		const { search } = req.query;
-		const suppliers = await suppliersController.getSuppliers(search);
-		res.json(suppliers);
-	} catch (error) {
-		next(error);
-	}
-});
-
 // check if email exists
 router.get("/check_email", async (req, res, next) => {
 	try {
@@ -42,6 +20,28 @@ router.get("/check_phone", async (req, res, next) => {
 		const { phone } = req.query;
 		const exists = await suppliersController.checkPhone(phone);
 		res.json(exists);
+	} catch (error) {
+		next(error);
+	}
+});
+
+// get supplier by id
+router.get("/:id", async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const supplier = await suppliersController.getSupplierById(id);
+		res.status(200).json(supplier);
+	} catch (error) {
+		next(error);
+	}
+});
+
+// search suppliers
+router.get("/", async (req, res, next) => {
+	try {
+		const { search } = req.query;
+		const suppliers = await suppliersController.getSuppliers(search);
+		res.json(suppliers);
 	} catch (error) {
 		next(error);
 	}
