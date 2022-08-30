26ba47e5e08781f50ff04f280f1a353d4ac6bf6c
LassazVegaz <lasindunuwanga@gmail.com>
2022-08-30 12:28:48 +0530

    increased body limit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const errorHandler = require("./middleware/errorHandlerMiddleware");
 
 const run = async () => {
 	const app = express();
-	app.use(express.json());
+	app.use(express.json({ limit: "50mb" }));
 	app.use(cookieParser());
 	app.use(cors());
 	app.use(
